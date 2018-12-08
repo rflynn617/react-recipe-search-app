0606969745c6d2947ee815912289f13bf7deb443
Robert Flynn <rflynn617@aol.com>
2018-12-08 17:56:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+// google api
+const API_KEY = "f7eb7b12c5b66b72dca3ef78b21334c6";
+
+//aol api
+//const API_KEY = "6bea2c4531519d3458b3d1bde39b1f76";
+// const URL =
+//   "https://www.food2fork.com/api/search?key={API_KEY}&q=shredded%20chicken";
+
+localStorage.clear();
 
 class App extends Component {
+  state = {
+    recipes: []
+  };
+  getRecipe = async e => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=12`
+    );
+
+    const data = await api_call.json();
+    this.setState({
+      recipes: data.recipes
+    });
+    // console.log(this.state.recipes);
+  };
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    // console.log(recipes);
+    this.setState({ recipes });
+  };
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  };
+  // localStorage.clear();
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Form = props => (
+  <form onSubmit={props.getRecipe} style={{ marginBottom: "2rem" }}>
+    <input
+      className="form__input"
+      type="text"
+      name="recipeName"
+      placeholder="Search Item or Ingredient"
+    />
+    <button className="form__button">Search</button>
+  </form>
+);
+
+export default Form;
